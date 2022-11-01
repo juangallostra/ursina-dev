6374bdf80d90a5d2e0c12d8fd6f15b8bb1e908a3
juangallostra <juangallostra@gmail.com>
2022-11-01 11:39:45 +0100

    chore: cleanup

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from ursina import *
-from controllers import DroneController, RotationController
+from controllers import DroneController
 
 DEFAULT_LINEAR_VELOCITY = 6
 DEFAULT_ANGULAR_VELOCITY = 120
@@ -11,18 +11,19 @@ class GenericPlayer(Entity):
         add_to_scene_entities=True, 
         linear_velocity=DEFAULT_LINEAR_VELOCITY, 
         angular_velocity=DEFAULT_ANGULAR_VELOCITY,
-        ground_height=0,
-        controller=RotationController,
+        controller=None,
         logger=None,
         **kwargs
     ):
         super().__init__(add_to_scene_entities, **kwargs)
+
         self._controller = controller(
             logger=logger,
             parent_entity=self,
             linear_velocity=linear_velocity,
             angular_velocity=angular_velocity,
-            ground_height=self.y)
+            ground_height=kwargs.get('ground_height', self.y)) # if no ground height has been specified, assume current y
+
         self._logger = logger
         self._collisions_againts = []
 

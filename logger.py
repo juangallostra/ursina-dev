7ce8b72319bbbb559f9164763913fb286d7c7113
juangallostra <juangallostra@gmail.com>
2022-11-02 15:43:19 +0100

    feature: enable logging to a file

--- logger.py
+++ logger.py
@@ -2,7 +2,7 @@ from ursina import *
 import datetime
 
 class Logger(Entity):
-    def __init__(self, messages_to_display, x_0, y_0, dy):
+    def __init__(self, messages_to_display, x_0, y_0, dy, write_to_log = True):
         super().__init__()
         self._message_queue = []
         self._messages_to_display = messages_to_display
@@ -10,6 +10,14 @@ class Logger(Entity):
         self._y0 = y_0 # .45
         self._dy = dy # -.05
         self._log_type = "LOG"
+        self._write_to_log = write_to_log
+
+    def _log_to_file(self, message):
+        if not self._write_to_log:
+            return
+        with open(f'run.log', 'a') as f:
+            f.write(f'{message}\n')
+
 
     def log(self, message):
         # if max log messages
@@ -22,6 +30,7 @@ class Logger(Entity):
         for msg in self._message_queue:
             msg.y += self._dy
         # enqueue and show new message
-        self._message_queue.append(Text(text=f'{self._log_type} [{datetime.datetime.now()}]: {message}', x=self._x0, y=self._y0))
-
+        log_message = f'{self._log_type} [{datetime.datetime.now()}]: {message}'
+        self._message_queue.append(Text(text=log_message, x=self._x0, y=self._y0))
+        self._log_to_file(log_message)
 
